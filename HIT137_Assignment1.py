8119ecff6331827ffaec835c97c8754d0ddf41d3
Samiakhan2003 <163192757+Samiakhan2003@users.noreply.github.com>
2024-03-25 23:46:04 +0930

    Add files via upload

---
+++ HIT137_Assignment1.py
@@ -0,0 +1,45 @@
+#Md Ashekor Rahnan Naim_s372251
+#Samia Akter_s365004
+#Isat Islam Mim_s375318
+#https://github.com/Samiakhan2003/desktop-tutorial/settings/access
+
+#Image processing: Introduction to the domain and its significance:
+#Image processing models have become experts in the rapidly developing artificial intelligence (AI), driving advancements in various fields. AI image processing is far quicker than a human body at analyzing. Convolutional neural networks (CNNs) and Generative adversarial networks (GANs) are two examples of these models linked with Healthcare: Artificial Intelligence's role in art and creativity, Surveillance, and Education. The importance of Python in AI development is examined in this essay, with a particular emphasis on image-processing models such as CNNs and GANs.
+
+#Artificial Intelligence in Healthcare for Image Processing:
+#In the healthcare industry, image processing is essential for several applications, including medical imaging, diagnosis, treatment planning, and monitoring. These are some necessary fields in which image processing is used,
+#Detecting Lung Cancer: One of the leading causes of death worldwide is lung cancer. Lung cancer claims the lives of more people each year than all other cancers combined. The average patient with lung cancer has an extremely short life expectancy. If the diagnosis is made in the early stages, the chances of patient survival are higher, which increases the patient survival rate, which is needed to detect cancer as early as possible. Image processing algorithms are employed to analyze chest X-rays, CT scans, and other imaging modalities for the early detection of lung diseases (Rahane et al., 2018).
+#Computer Aid Diagnosis: Through the automatic interpretation of medical images and the identification of patterns or features suggestive of particular diseases or disorders, Computer Aid Diagnosis (CAD) systems offer diagnostic support. Improved patient outcomes result from radiologists and doctors using CADx tools to help them make rapid and accurate diagnostic choices. CAD systems have started to become common in recent years to diagnose and phase several types of cancer, such as lung, brain, and breast cancer, in parallel with the developments of medical imaging and computer technologies. Our study recommends a computer-aided diagnosis system for detecting and diagnosing stomach cancer using endoscopy images (Yasar et al., 2019).
+
+#Artificial Intelligence Image Recognition: Unleashing Creativity and Efficiency:
+#Artificial image and scene recognition have advanced significantly because AI image processing has changed how photographers arrange, classify, and find their photos.
+#AI image processing algorithms have eliminated the need to categorize each image according to its subject or substance manually. With remarkable precision, AI image recognition technology can now recognize objects, people, landmarks, and scenes in photos. This innovative functionality saves photographers time and effort by swiftly searching for specific photographs based on their content.
+#Photographers may simplify image organization and search capabilities with AI-driven image labeling, categorization, and searchability (Aithority, 2023).
+
+#Artificial Intelligence in Surveillance for Image Processing:
+#Image processing models are crucial for several tasks in AI-powered surveillance, including tracking, anomaly detection, object detection, and recognition. Some of the most essential image-processing models found in AI-based surveillance systems are as follows:
+#CNNs or convolutional neural networks: Most contemporary image-processing jobs are powered by CNNs. They excel in extracting hierarchical features from pictures. CNNs are used in surveillance to perform segmentation, classification, and object detection (Mehra, 2023).
+#Convolutional neural networks based on regions (R-CNNs): In surveillance, R-CNNs and their variations—Fast R-CNN, Faster R-CNN, Mask R-CNN—are widely used for object detection tasks. These models categorize and localize objects in an image into pre-established groups (Mehra, 2023).
+
+#Impact on society and ethical considerations in AI:
+#Using DL techniques, such as CNNs and GANs, in medical imaging has increased ethical considerations. Using these techniques can decrease the danger of data breaches and cyberattacks while protecting patient privacy. Moreover, it can support integrating DL models into healthcare workflows and make them more transparent and interpretable. An additional essential factor to consider when using DL in medical imaging is how it may be integrated into clinical workflows. DL models must be successfully, reliably, and efficiently integrated into clinical workflows to be therapeutically valuable. Among the many factors that need careful consideration are the forecasts' accuracy and application, the availability and accessibility of data, and the effect on clinical decision-making (Zhang & Qie, 2023).
+
+#Available tools and libraries to Python for developing image processing models:
+#A collection of resources for image processing called Sci-kit-image provides tools and techniques for usage in academic, commercial, and research settings. It offers a well-documented API in the Python programming language. It is developed by an active multinational team of collaborators and is made available under the welcoming Modified BSD free access license (Ven der Walt et al., 2014).
+
+#Conclusion: In the final analysis, there are many applications for image processing models, such as CNNs and GANs, from object recognition to image synthesis. Despite many benefits, their employment raises serious ethical questions about discrimination, privacy, and the destruction of jobs. Python provides a rich ecosystem of tools and libraries to effectively develop and deploy these models.
+
+
+#Reference:
+
+#1. Aithority AI Technology Insights. (2023). Machine Learning & Modern Photography: Revolutionizing Visual Art with AI Image Processing. AIT Staff Writer. https://aithority.com/machine-learning/revolutionizing-visual-art-with-ai-image-processing/
+
+#2. Mehra, A. (2023, March 28). Automating Surveillance Using Image Classification AI Model. Lebellerr. https://www.labellerr.com/blog/automating-surveillance-using-image-classification-ai-model/
+
+#3. Rahane, W., Dalvi, H., Magar, Y., Kalane, A., & Jondhale, S. (2018). Lung Cancer Detection Using Image Processing and Machine Learning HealthCare. 2018 International Conference on Current Trends towards Converging Technologies (ICCTCT). https://ieeexplore.ieee.org/document/8551008 
+
+#4. Ven der Walt, S., Johannes, L., Nunez-Iglesias, J., Boulogne, F., Warner, J. D., Yager, N., Gouillart, E., Yu, T. (2014). Scikit-image: image processing in Python. PeerJ, https://peerj.com/articles/453/?report=reader&utm_source=TrendMD&utm_campaign=PeerJ_TrendMD_1&utm_medium=TrendMD
+
+#5. Yasar, A., Saritas, I., & Korkmaz, H. (2019). Computer-Aided Diagnosis System for Detection of Stomach Cancer with Image Processing Techniques. Journal of Medical Systems, 43(4). https://doi.org/10.1007/s10916-019-1203-y  
+
+#6. Zhang, H., & Qie, Y. (2023). Applying Deep Learning to Medical Imaging: A Review. Applied Sciences, 13(18), 10521. https://doi.org/10.3390/app131810521
